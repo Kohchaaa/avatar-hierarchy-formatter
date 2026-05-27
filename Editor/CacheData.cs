d814bc7d73f0fee2b38c0ce5eb53a5800e10e4f4
Kohchaaa <koustar2004@gmail.com>
2026-05-27 10:05:12 +0900

    feat: cacheにコンポーネントの情報を持たせるように

--- Editor/CacheData.cs
+++ Editor/CacheData.cs
@@ -1,20 +1,36 @@
-public struct CacheData
+using UnityEngine;
+
+namespace Kohcha.AvatarHierarchyFormatter
 {
-    public int AvatarRootId;
-    
-    public int IndentLevel;
-    public bool IsLastChild;
-
-    public bool[] ParentLineFlags;
-
-    public bool HasChildren;
-
-    public CacheData(int id, int indentLevel, bool isLastChild, bool[] flags, bool hasChildren)
+    public struct CacheData
     {
-        AvatarRootId = id;
-        IndentLevel = indentLevel;
-        IsLastChild = isLastChild;
-        ParentLineFlags = flags;
-        HasChildren = hasChildren;
+        public int AvatarRootId;
+
+        public int IndentLevel;
+        public bool IsLastChild;
+
+        public bool[] ParentLineFlags;
+
+        public bool HasChildren;
+
+        public CacheData(int id, int indentLevel, bool isLastChild, bool[] flags, bool hasChildren)
+        {
+            AvatarRootId = id;
+            IndentLevel = indentLevel;
+            IsLastChild = isLastChild;
+            ParentLineFlags = flags;
+            HasChildren = hasChildren;
+        }
+    }
+
+    public struct ComponentIconInfo
+    {
+        public Texture2D Icon;
+        public bool IsEnabled;
+        public bool CanToggle;
+        public bool IsMissing;
+        public int InstanceID;
     }
 }
+
+
