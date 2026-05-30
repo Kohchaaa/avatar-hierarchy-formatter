8bf518d92c66b9c9695bf1abf11cec5090204664
Kohchaaa <koustar2004@gmail.com>
2026-05-30 19:38:28 +0900

    feat: ComponentIconに使う各種設定を追加

--- Editor/Setting/Modules/ComponentIconSettingModue.cs
+++ Editor/Setting/Modules/ComponentIconSettingModue.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEditor;
 
 namespace Kohcha.AvatarHierarchyFormatter
@@ -12,19 +13,43 @@ namespace Kohcha.AvatarHierarchyFormatter
         private const string Key_Enabled = "Enabled";
         public static bool IsEnabled = true;
 
+        private const string Key_IconOffset = "IconOffset";
+        public static float IconOffset = 0f;
+
+        private const string Key_AllowToggleFromIcon = "AllowToggleFromIcon";
+        public static bool IsAllowToggleFromIcon = true;
+
+        private const string Key_AllowGrouping = "AllowGrouping";
+        public static bool IsAllowGrouping = true;
+
+        private const string Key_AllowCustomIcon = "AllowCustomIcon";
+        public static bool IsAllowCustomIcon = true;
+
         public void Load()
         {
-            this.LoadBool(Key_Enabled, true);
+            IsEnabled = this.LoadBool(Key_Enabled);
+            IconOffset = this.LoadFloat(Key_IconOffset);
+            IsAllowToggleFromIcon = this.LoadBool(Key_AllowToggleFromIcon);
+            IsAllowGrouping = this.LoadBool(Key_AllowGrouping);
+            IsAllowCustomIcon = this.LoadBool(Key_AllowCustomIcon);
         }
 
         public void Save()
         {
             this.SaveBool(Key_Enabled, IsEnabled);
+            this.SaveFloat(Key_IconOffset, IconOffset);
+            this.SaveBool(Key_AllowToggleFromIcon, IsAllowToggleFromIcon);
+            this.SaveBool(Key_AllowGrouping, IsAllowGrouping);
+            this.SaveBool(Key_AllowCustomIcon, IsAllowCustomIcon);
         }
-        
+
         public void OnGUI()
         {
             IsEnabled = EditorGUILayout.Toggle("有効化", IsEnabled);
+            IconOffset = EditorGUILayout.FloatField("アイコンのオフセット", IconOffset);
+            IsAllowToggleFromIcon = EditorGUILayout.Toggle("アイコンクリックでトグルを許可", IsAllowToggleFromIcon);
+            IsAllowGrouping = EditorGUILayout.Toggle("アイコンのグループ化を許可", IsAllowGrouping);
+            IsAllowCustomIcon = EditorGUILayout.Toggle("カスタムアイコンを使用", IsAllowCustomIcon);
         }
     }
 }
