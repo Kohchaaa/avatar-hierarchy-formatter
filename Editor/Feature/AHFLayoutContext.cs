abddf40131be76df5b4a7799b9d5bf8b7ce5d10d
Kohchaaa <koustar2004@gmail.com>
2026-06-03 21:17:52 +0900

    feat: 右並べる用のオフセットとか持ったコンテキスト構造体の作成

---
+++ Editor/Feature/AHFLayoutContext.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Kohcha.AvatarHierarchyFormatter
+{
+    public struct AHFLayoutContext
+    {
+        public readonly int InstanceID;
+        public readonly Rect SelectionRect;
+
+        public AHFRightOffsetEngine RightOffset;
+
+        public AHFLayoutContext(int instanceID, Rect selectionRect, float initialOffset)
+        {
+            InstanceID = instanceID;
+            SelectionRect = selectionRect;
+
+            RightOffset = new AHFRightOffsetEngine(initialOffset);
+        }
+    }
+
+    // 右から並べる用のオフセットと計算できるやつ持ってる構造体
+    public struct AHFRightOffsetEngine
+    {
+        public float CurrentOffset { get; private set; }
+
+        public AHFRightOffsetEngine(float initialOffset)
+        {
+            CurrentOffset = initialOffset;
+        }
+
+        public Rect AllocateRect(Rect selectionRect, float width, float paddingAfter)
+        {
+            Rect rect = new Rect(
+                selectionRect.xMax - CurrentOffset - width,
+                selectionRect.y,
+                width,
+                selectionRect.height
+            );
+
+            CurrentOffset += width + paddingAfter;
+            return rect;
+        }
+    }
+}
