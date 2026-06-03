0301be5f1e85fc5c67c77a2b98523f4e285e6277
Kohchaaa <koustar2004@gmail.com>
2026-06-04 00:58:22 +0900

    feat: toggleButtonのオフセットを追加

--- Editor/Setting/Modules/ToggleActiveSettingModule.cs
+++ Editor/Setting/Modules/ToggleActiveSettingModule.cs
@@ -14,22 +14,28 @@ namespace Kohcha.AvatarHierarchyFormatter
         private const string Key_Enabled = "Enabled";
         public static bool IsEnabled = true;
 
+        private const string Key_ButtonOffset = "ButtonOffset";
+        public static float ButtonOffset = 0f;
+
         public void Load()
         {
             // 有効化
-            IsEnabled = this.LoadBool(Key_Enabled, true);
+            IsEnabled = this.LoadBool(Key_Enabled);
+            ButtonOffset = this.LoadFloat(Key_ButtonOffset);
         }
 
         public void Save()
         {
             // 有効化
             this.SaveBool(Key_Enabled, IsEnabled);
+            this.LoadFloat(Key_ButtonOffset, ButtonOffset);
         }
 
         public void OnGUI()
         {
             // 有効化
             IsEnabled = EditorGUILayout.Toggle("有効化", IsEnabled);
+            ButtonOffset = EditorGUILayout.FloatField("ボタンのオフセット", ButtonOffset);
         }
 
     }
