97f56edbb1922f896e2a5f0b2de0335091f00cce
Kohchaaa <koustar2004@gmail.com>
2026-06-04 11:55:33 +0900

    feat: EditorOnly切り替え機能の設定クラス作成

---
+++ Editor/Setting/Modules/ToggleEOSettingModule.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+
+namespace Kohcha.AvatarHierarchyFormatter
+{
+    [InitializeOnLoad]
+    public class ToggleEOSettingModule : IAHFSettingModule
+    {
+        public string LabelName => "エディターオンリートグル";
+        public string ModuleName => "ToggleEO";
+
+        //=========================================================
+        // キー
+        // 有効化
+        private const string Key_Enabled = "Enabled";
+        public static bool IsEnabled = true;
+
+        private const string Key_ButtonOffset = "ButtonOffset";
+        public static float ButtonOffset = 0f;
+
+        public void Load()
+        {
+            // 有効化
+            IsEnabled = this.LoadBool(Key_Enabled);
+            ButtonOffset = this.LoadFloat(Key_ButtonOffset);
+        }
+
+        public void Save()
+        {
+            // 有効化
+            this.SaveBool(Key_Enabled, IsEnabled);
+            this.LoadFloat(Key_ButtonOffset, ButtonOffset);
+        }
+
+        public void OnGUI()
+        {
+            // 有効化
+            IsEnabled = EditorGUILayout.Toggle("有効化", IsEnabled);
+            ButtonOffset = EditorGUILayout.FloatField("ボタンのオフセット", ButtonOffset);
+        }
+    }
+}
